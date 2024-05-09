c704c01a835d05063ef52a6c4b98bd7411c1aafd
Marco Maues <marcomauesjr@gmail.com>
2024-05-09 12:16:57 -0300

    Resultados do ag szz issue filter

--- lerjson.py
+++ lerjson.py
@@ -1,7 +1,7 @@
 import json
 
-with open('consensus_SZZ/out/bic_bszz_1715084601.json', 'r') as file:
+with open('consensus_SZZ/out/bic_agszz_issues_filter_1715256613.json', 'r') as file:
     data = json.load(file)
 novo_data = data
-with open('consensus_SZZ/out/jenkins_bssz.json','w') as file:
+with open('consensus_SZZ/out/jenkins_agssz_issue_filter.json','w') as file:
     json.dump(novo_data,file, indent=4)
