acdaad64203264fd534d12c7b6e78ba3bf574ae3
Marco Maues <marcomauesjr@gmail.com>
2024-05-21 12:39:19 -0300

    atualização da leitura do json

--- lerjson.py
+++ lerjson.py
@@ -1,7 +1,8 @@
 import json
 
-with open('consensus_SZZ/out/bic_agszz_issues_filter_1715256613.json', 'r') as file:
+with open('/home/marco/Documentos/decision_szz/pyszz_v2/out/bic_lszz_1715780238.json', 'r') as file:
     data = json.load(file)
 novo_data = data
-with open('consensus_SZZ/out/jenkins_agssz_issue_filter.json','w') as file:
+with open('/home/marco/Documentos/decision_szz/pyszz_v2/out/example3l.json','w') as file:
     json.dump(novo_data,file, indent=4)
+
